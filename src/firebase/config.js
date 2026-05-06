2e78d13e40172eb0a54fcb79a4dd88e2c4721c44
OGOR-STEPHEN <ogorstephen485@gmail.com>
2026-05-06 17:19:22 +0100

    final push

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB32VD_wMNpHUCrw_bJBBpHVlJmLfzd0lg",
-  authDomain: "time-management-app-8a7d3.firebaseapp.com",
-  projectId: "time-management-app-8a7d3",
-  storageBucket: "time-management-app-8a7d3.firebasestorage.app",
-  messagingSenderId: "88077950402",
-  appId: "1:88077950402:web:5b52fe25b80d151b9a4a5b",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
